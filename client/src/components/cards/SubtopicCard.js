2d157f4923708c95d557ffb19c5d663b48aaa306
vsikka02 <vsikka02@gmail.com>
2020-04-17 15:54:25 -0500

    subtopicCards redesigned and restructured

--- client/src/components/cards/SubtopicCard.js
+++ client/src/components/cards/SubtopicCard.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "./SubtopicCard.css";
 import Textfit from "react-textfit"
+import { requirePropFactory } from "@material-ui/core";
 
 class SubtopicCard extends Component {
   render() {
@@ -9,6 +10,7 @@ class SubtopicCard extends Component {
       <div>
         <div class="col-md-4 subtopic">
           <div class="card card-1">
+          <img className = "logo" src ={this.props.photoUrl}/>
             <div id="title-and-desc">
               <h3 className="subtopic-card-name">{this.props.name}</h3>
               <p className="subtopic-card-description">{this.props.description}</p>
@@ -26,5 +28,6 @@ SubtopicCard.propTypes = {
   subtopicId: PropTypes.string,
   topicId: PropTypes.string,
   description: PropTypes.string,
+  photoUrl: PropTypes.string
 };
 export default SubtopicCard;

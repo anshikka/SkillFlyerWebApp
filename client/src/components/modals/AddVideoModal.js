d121b1a7020ed350410045e424c6c2f2a0a83816
Ansh Sikka <sikka008@umn.edu>
2020-06-22 14:43:37 -0500

    Finished up frontend interface of adding a video

--- client/src/components/modals/AddVideoModal.js
+++ client/src/components/modals/AddVideoModal.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Modal from "@material-ui/core/Modal";
-import FormControl from "@material-ui/core/FormControl";
 import PropTypes from "prop-types";
 import Button from "@material-ui/core/Button";
 import CloudUploadIcon from "@material-ui/icons/CloudUpload";

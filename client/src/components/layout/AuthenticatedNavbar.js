05b04f92b84e170a3a8e6a0d375cf48952ec17a3
Vansh Sikka <vanshsikka@MacBook-Pro.local>
2020-06-29 15:53:47 -0500

    user drop down menu basic front end complete

--- client/src/components/layout/AuthenticatedNavbar.js
+++ client/src/components/layout/AuthenticatedNavbar.js
@@ -7,13 +7,26 @@ import PersonRoundedIcon from "@material-ui/icons/PersonRounded";
 import InputBase from "@material-ui/core/InputBase";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import PropTypes from "prop-types";
 
 class AuthenticatedNavbar extends Component {
-  onLogoutClick = (e) => {
-    e.preventDefault();
-    this.props.logoutUser();
+
+  state = { anchorEl: false };
+
+
+  handleClick = () => {
+    this.setState(() => ({ anchorEl: true }));
+    console.log("opened")
   };
+  handleClose = () => {
+    this.setState(() => ({ anchorEl: false }));
+    console.log("close")
+  };
+
+
   render() {
     var logo;
     if (this.props.color === "white") {
@@ -22,6 +35,7 @@ class AuthenticatedNavbar extends Component {
       logo = darkLogo;
     }
     const { user } = this.props.auth;
+
 
     return (
       <AppBar id="appbar-auth" position="static">
@@ -36,10 +50,23 @@ class AuthenticatedNavbar extends Component {
               inputProps={{ "aria-label": "search" }}
             ></InputBase>
           </div>
-          <a href="/logout" onClick={this.onLogoutClick} id="navbar-user-link">
+          <div className = "user-menu-root">
+          <Button className = "user-menu-button" aria-controls="simple-menu" aria-haspopup="true" onClick={this.handleClick}>
             <PersonRoundedIcon className="navbar-icon" />
             <figcaption>{user.name.split(" ")[0]}</figcaption>
-          </a>
+          </Button>
+          <Menu
+            className = "user-menu"
+            id="simple-menu"
+            anchorEl={this.state.anchorEl}
+            open={this.state.anchorEl}
+            onClose={this.handleClose}
+          >
+            <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+            <MenuItem onClick={this.handleClose}>My account</MenuItem>
+            <MenuItem onClick={this.props.logoutUser}>Logout</MenuItem>
+          </Menu>
+          </div>
         </Toolbar>
       </AppBar>
     );

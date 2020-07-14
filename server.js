3b65be90093d7f1af9def5755abc0519213e897f
Ansh Sikka <sikka008@umn.edu>
2020-07-14 15:02:36 -0500

    fixed router

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(
 );
 app.use(bodyParser.json());
 
-app.use(express.static('./client/build'));
+app.use(express.static('client/build'));
 
 // DB Config
 const db = require("./config/keys").mongoURI;

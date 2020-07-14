3a9ec4b6e978f8cdc75ff714268d7037ef1c1a59
Ansh Sikka <sikka008@umn.edu>
2020-07-14 14:17:53 -0500

    Adding backend files

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://skillflyer_user:Mukti@54321@skillflyerdev-siafg.azure.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret",
+};
---
+++ models/Subtopic.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const SubtopicSchema = new Schema({
+  subtopic_name: {
+    type: String,
+    required: true,
+  },
+  topic_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "topics",
+    required: true,
+  },
+  added_by: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  photo_url: {
+    type: String,
+    required: true,
+  },
+});
+module.exports = Subtopic = mongoose.model("subtopics", SubtopicSchema);
